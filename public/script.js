5c4eb6a6f25c51397711cf0183ac2a07c88c761f
Leah-Grace <leahgdg@hotmail.com>
2019-05-20 19:08:14 -0400

    switch to mongo branch

--- public/script.js
+++ public/script.js
@@ -1,1 +1,40 @@
 console.log(`Script runnung`);
+
+function GETUserInfo() {
+    const input = document.getElementById("getuserInfo").value;
+    const url = "/showprofile/" + input;
+    //axios .get
+
+    axios.get(url)
+    .then(response => {
+        console.log(response.data);
+        document.getElementById("userInfo").innerText = response.data;
+    });
+}
+
+function POSTUserInfo() {
+    const userName = document.getElementById("user-name").value;
+    const userId = document.getElementById("user-id").value;
+    const message = document.getElementById("message").value;
+//console.log(`${userName} ${userId} ${message}`);
+   
+const payload = {
+    userName,
+    userId,
+    message
+}
+
+     axios.post("/api/", payload)
+    .then(response => {
+        console.log(response.data);
+        document.getElementById("userInfo").innerText = response.data;
+    });
+}
+
+function getAllUsers() {
+    axios.get("getallusers")
+    .then(response => {
+        document.getElementById("allUserResult").innerHTML = JSON.stringify(response.data);
+    });
+}
+
--- server.js
+++ server.js
@@ -1,22 +1,51 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
-const port = 3000;
+const port = 3030;
 
+app.use(bodyParser.json());
 app.use('/', express.static("public"))
 
 //This route already used
-app.get('/', (req, res) => res.send('Hello You, Moron!'))
+//app.get('/', (req, res) => res.send('Hello You, Moron!'))
 
 //A query requires a key value pair but a parameter does not!
 //to send multiple queries
-app.get('/api', function(req, res){
-    const userName = req.query.username;
-    const userId = req.query.id;
-    const message = req.query.message;
-    const reply = `${userName} ${userId} ${message}`
-    res.send(`${reply}`);
-    console.log(userName);
+// app.get('/api', function(req, res){
+//     const userName = req.query.username;
+//     const userId = req.query.id;
+//     const message = req.query.message;
+//     const reply = `${userName} ${userId} ${message}`
+//     res.send(`${reply}`);
+//     console.log(userName);
+// })
+
+
+const data = [];
+
+//set up a route to accept post requests
+app.post('/api/', function(req, res){
+    const userName = req.body.userName;
+    const userId = req.body.userId;
+    const message = req.body.message;
+    const reply = `"${userName} with id of ${userId} is saying ${message}"`;
+    res.send(reply);
+    console.log(reply);
+    const temp = {
+        userName,
+        userId,
+        message
+    }
+    data.push(temp);
+
+    console.log(data);
 })
+
+app.get("/getallusers", function (req, res) {
+    res.send(data);
+})
+
+
 //addGET request that sends parameters
 app.get('/showprofile/:userName', function(req, res){
 const user = req.params.userName;
@@ -24,7 +53,7 @@ console.log(user);
     res.send(`"showprofile" for ${user} is working!`)
 })
 
-
+////testing Nodemon
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 //you can send querys and oarameters on a request
